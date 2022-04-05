7af2c382de2569b8e4f45c81fdb1d40ca4395c5d
Noelma Carme Gondim de Oliveira <noelmagondim@gmail.com>
2022-04-05 16:43:48 -0300

    Iniciando requisito 03

---
+++ src/pages/Configuracoes.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Configuracoes extends Component {
+  render() {
+    return (
+      <div>Iniciando Desafio 03</div>
+    );
+  }
+}
+
+export default Configuracoes;
