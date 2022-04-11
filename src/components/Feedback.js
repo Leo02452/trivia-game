2434c255f737f449fd4ba5a418c059de29e9b697
Noelma Carme Gondim de Oliveira <noelmagondim@gmail.com>
2022-04-11 15:19:10 -0300

    Req 13: criado a lógica, mas ainda não foi testado

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Header from './Header';
 
 class Feedback extends Component {
   render() {
+    const { assertions } = this.props;
+    const auxNumber = 3;
     return (
-      <main>
-        <Header />
+      <div>
+        <main>
+          <Header />
+        </main>
+        {/* Abaixo Desafio 13 */}
+        <h1
+          data-testid="feedback-text"
+        >
+          {assertions >= auxNumber ? 'Well Done!' : 'Could be better...'}
+        </h1>
         <section>
           {/* Abaixo Desafio 15 */}
           <Link to="/">
@@ -28,9 +39,17 @@ class Feedback extends Component {
             </button>
           </Link>
         </section>
-      </main>
+      </div>
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = ({
+  assertions: PropTypes.number.isRequired,
+});
+
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+});
+
+export default connect(mapStateToProps, null)(Feedback);
